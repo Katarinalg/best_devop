dfbc10acba9f9b872ab0f9358627f17957e97bf0
katarinalg <katarina@pc7.dev>
2024-07-02 10:58:42 +0200

    added os and versions

--- test_main.py
+++ test_main.py
@@ -7,5 +7,10 @@ def test_add():
     assert add(3, 4) == 7
     assert (add(np.array([1, 2]), np.array([3, 4])) == np.array([4, 6])).all()
     assert (add(np.array([1, 2]), 3) == np.array([4, 5])).all()
+    assert add(2, 3) == 5
+    assert add(2.0, 3.0) == 5.0
+    assert (add(np.array([2, 3]), np.array([4, 5])) == np.array([6, 8])).all()
+    
+
 
 # TODO: add more tests
